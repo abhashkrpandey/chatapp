e9ca11ef96c1d8d61b68feebc8ff850ed7af7cb3
Abhash Pandey <abhashkrpandey4@gmail.com>
2024-10-19 23:44:21 +0530

    li

--- backend/middleware/mw1.js
+++ backend/middleware/mw1.js
@@ -4,7 +4,7 @@ function profileAuthentication(req,res,next)
     jwt.verify(req.cookies.token,process.env.SECRETKEY,(err,decoded)=>{
          if(err)
          {
-            res.send({redirect:"/notidentified"});
+            res.send({redirect:"/notidentified",error:err});
          }
          else{
             req.decoded=decoded;
